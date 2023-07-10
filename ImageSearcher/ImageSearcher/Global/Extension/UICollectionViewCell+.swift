1cc815361b6412f42edaa51580a349e31a10075c
juhyeon <wngus4296@swu.ac.kr>
2023-07-10 20:07:59 +0900

    [FIX] 피드백 반영: className extension으로 변경

---
+++ ImageSearcher/ImageSearcher/Global/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,12 @@
+//
+//  UICollectionViewCell+.swift
+//  ImageSearcher
+//
+//  Created by Juhyeon Byun on 2023/07/10.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static let className = String(describing: UICollectionView.self)
+}
---
+++ ImageSearcher/ImageSearcher/Global/Extension/UIViewController+.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewController+.swift
+//  ImageSearcher
+//
+//  Created by Juhyeon Byun on 2023/07/10.
+//
+
+import UIKit
+
+extension UIViewController {
+    static let className = String(describing: UIViewController.self)
+}
